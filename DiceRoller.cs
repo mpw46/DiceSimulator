dac26d274b6f48919c62ca6f8ba8379d9ad8bdec
mpw46 <mpw46@byu.edu>
2026-01-14 10:07:22 -0700

    Comments

--- DiceRoller.cs
+++ DiceRoller.cs
@@ -8,11 +8,13 @@ namespace DiceSimulator
     {
         public static int[] Roll(int numRolls)
         {
+            // Initialize variables
             int num1 = 0;
             int num2 = 0;
             int total = 0;
             int[] counts = new int[13];
 
+            // Simulate as many dice rolls as the user inputs and count in array
             for (int iCount = 0; iCount < numRolls; iCount++)
             {
                 num1 = Random.Shared.Next(1, 7);
@@ -21,6 +23,7 @@ namespace DiceSimulator
                 counts[total] = counts[total] + 1;
             }
 
+            // Return the counts array
             return counts;
         }
 
--- Program.cs
+++ Program.cs
@@ -4,10 +4,12 @@ internal class Program
 {
     public static void Main(string[] args)
     {
+        // Initialize variables
         int numRolls = 0;
         int[] results = new int[13];
         int total = 0;
 
+        // Write welcome message and prompt user for number of rolls
         Console.WriteLine("Welcome to the dice throwing simulator!");
         Console.WriteLine("\nHow many dice rolls would you like to simulate?");
         numRolls = int.Parse(Console.ReadLine());
@@ -15,14 +17,17 @@ internal class Program
             "Each \"*\" represents 1% of the total number of rolls.\n" +
             "Total number of rolls = " + numRolls + ".\n");
 
+        // Roll the dice and get results
         results = DiceRoller.Roll(numRolls);
 
+        // Print results with an asterisk per percentage point
         for (int iCount = 2; iCount < 13; iCount++)
         {
             int percentage = (int)Math.Round((double)results[iCount] / numRolls * 100);
             Console.WriteLine(iCount + ": " + new string('*', percentage));
         }
 
+        // Print closing message
         Console.WriteLine("\nThank you for using the dice throwing simulator. Goodbye!");
     }
 }
